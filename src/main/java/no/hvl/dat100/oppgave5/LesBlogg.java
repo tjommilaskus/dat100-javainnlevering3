e393c50031f9a912348a32cc6903b7c9d329bf35
tjommilaskus <mps.94@me.com>
2024-11-07 14:21:34 +0100

    Finner ikke losning paa 5

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -5,55 +5,58 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
-import no.hvl.dat100.common.TODO;
-import no.hvl.dat100.oppgave1.*;
-import no.hvl.dat100.oppgave2.*;
-import no.hvl.dat100.oppgave3.*;
-
-import javax.swing.JOptionPane;
+import no.hvl.dat100.oppgave1.Bilde;
+import no.hvl.dat100.oppgave1.Innlegg;
+import no.hvl.dat100.oppgave1.Tekst;
+import no.hvl.dat100.oppgave3.Blogg;
 
 public class LesBlogg {
 
-	private static String TEKST = "TEKST";
-	private static String BILDE = "BILDE";
+    private static final String TEKST = "TEKST";
+    private static final String BILDE = "BILDE";
 
-	public static Blogg les(String mappe, String filnavn) {
-		try {
-			File fil = new File(mappe, filnavn);
+    public static Blogg les(String mappe, String filnavn) {
+        try {
+       
+            File fil = new File(mappe, filnavn);
 
-			BufferedReader reader = new BufferedReader(new FileReader(fil));
+        
+            BufferedReader reader = new BufferedReader(new FileReader(fil));
 
-			int antallInnlegg = Integer.parseInt(reader.readLine());
+         
+            int antallInnlegg = Integer.parseInt(reader.readLine());
 
-			Blogg blogg = new Blogg(antallInnlegg);
+       
+            Blogg blogg = new Blogg(antallInnlegg);
 
-			for (int i = 0; i < antallInnlegg; i++) {
-				String type = reader.readLine();
-				int id = Integer.parseInt(reader.readLine());
-				String bruker = reader.readLine();
-				String dato = reader.readLine();
-				int likes = Integer.parseInt(reader.readLine());
+         
+            for (int i = 0; i < antallInnlegg; i++) {
+                String type = reader.readLine();
+                int id = Integer.parseInt(reader.readLine());
+                String bruker = reader.readLine();
+                String dato = reader.readLine();
+                int likes = Integer.parseInt(reader.readLine());
 
-				if (type.equals(TEKST)) {
-					String tekst = reader.readLine();
-					Innlegg innlegg = new Tekst(id, bruker, likes, tekst);
-					blogg.leggTil(innlegg);
-				} else if (type.equals(BILDE)) {
-					String tekst = reader.readLine();
-					String url = reader.readLine();
-					Innlegg innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
-					blogg.leggTil(innlegg);
-				}
-			}
-			reader.close();
-			return blogg;
-		} catch (IOException e) {
-			System.err.println("Feil ved lesning fra fil: " + e.getMessage());
-			return null;
-		}
+                if (type.equals(TEKST)) {
+                    String tekst = reader.readLine();
+                    Innlegg innlegg = new Tekst(id, bruker, dato, likes, tekst);
+                    blogg.leggTil(innlegg);
+                } else if (type.equals(BILDE)) {
+                    String tekst = reader.readLine();
+                    String url = reader.readLine();
+                    Innlegg innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
+                    blogg.leggTil(innlegg);
+                }
+            }
 
-	}
+           
+            reader.close();
+
+            return blogg;
+        } catch (IOException e) {
+            System.err.println("Feil ved lesing fra fil: " + e.getMessage());
+            return null;
+        }
+    }
 }
